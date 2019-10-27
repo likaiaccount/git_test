3da33f6fb46496658974a5f6eac16ac4a4655e36
likai <724267271@qq.com>
2019-10-28 01:23:56 +0800

    第二次修改

--- Git_Repository/src/com/itheima/FirstClass.java
+++ Git_Repository/src/com/itheima/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello,我来了");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
