c6ff2aee7a7d332919fa61e9948a6736543aa0af
likai <724267271@qq.com>
2019-10-28 00:56:47 +0800

    把本地工程添加到git

--- Git_Repository/src/com/itheima/FirstClass.java
+++ Git_Repository/src/com/itheima/FirstClass.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello,我来了");
+        System.out.println("第一次修改");
     }
 }
